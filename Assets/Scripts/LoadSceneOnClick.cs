875aa4b77500d39f1977eb71cb48fba2ab39e8f0
syikin <syikinalwi@gmail.com>
2018-04-07 05:08:41 +0800

    done panel pluto scan

---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+	public void LoadByIndex(int sceneIndex)
+	{
+		SceneManager.LoadScene (sceneIndex);
+
+	}
+
+}
